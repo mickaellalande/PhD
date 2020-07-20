44f3afc3c1c01e20ce5f682510e3bfba9605ee4c
Mickaël <doremiska@gmail.com>
2020-07-20 11:53:33 +0200

    Follow tas and add envs + bugs

--- envs/README.md
+++ envs/README.md
@@ -5,9 +5,30 @@ https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environme
 - For installing on HPC: https://github.com/mickaellalande/MC-Toolkit/tree/master/conda_environment_xarray_xesmf_proplot
 - My xarray environment: https://github.com/mickaellalande/MC-Toolkit/tree/master/conda_environment_xarray_xesmf_proplot/xarray
 
+
+```bash
+conda env list
+
+conda create --name myenv
+conda create --name myclone --clone myenv
+
+conda list --explicit > spec-file.txt
+conda create --name myenv --file spec-file.txt
+
+
+conda env export > environment.yml
+conda env export --from-history > environment.yml
+conda env create -f environment.yml
+```
+
+
 ---
 
 ## PhD environments
 
 - work_v1 (made the 24/06/2020 on Jean-Zay)
 - work (made on CICLAD -> didn't work on Jean-Zay?)
+
+## Proplot environments
+
+- proplot_v0.6.4 (20/07/2020 for testing bar bug)
--- utils/zones.py
+++ utils/zones.py
@@ -19,9 +19,9 @@ def get_domain_HMA():
 # HM: Central and Est Himalaya
 # TB: Tibetan Plateau
 def get_zones():
-    lonlim_HK = (70, 81 ); latlim_HK = (31, 40)
-    lonlim_HM = (79, 98 ); latlim_HM = (26, 31)
-    lonlim_TP = (81, 104); latlim_TP = (31, 39)
+    lonlim_HK = slice(70, 81 ); latlim_HK = slice(31, 40)
+    lonlim_HM = slice(79, 98 ); latlim_HM = slice(26, 31)
+    lonlim_TP = slice(81, 104); latlim_TP = slice(31, 39)
     
     return lonlim_HK, latlim_HK, lonlim_HM, latlim_HM, lonlim_TP, latlim_TP
 
@@ -30,31 +30,31 @@ def plot_zones(ax):
     lonlim_HK, latlim_HK, lonlim_HM, latlim_HM, lonlim_TP, latlim_TP = get_zones()
     
     # HKK
-    ax.text(lonlim_HK[0]+0.5, latlim_HK[1]-2, 'HKK', zorder=10)
+    ax.text(lonlim_HK.start+0.5, latlim_HK.stop-2, 'HKK', zorder=10)
     ax.add_patch(
         mpatches.Rectangle(
-            xy = [lonlim_HK[0], latlim_HK[0]], 
-            width = lonlim_HK[1]-lonlim_HK[0], height = latlim_HK[1]-latlim_HK[0],
+            xy = [lonlim_HK.start, latlim_HK.start], 
+            width = lonlim_HK.stop-lonlim_HK.start, height = latlim_HK.stop-latlim_HK.start,
             transform = ccrs.PlateCarree(), fill = False, zorder=10
         )
     )
 
     # HM
-    ax.text(lonlim_HM[0]+0.5, latlim_HM[1]-2, 'HM', zorder=10)
+    ax.text(lonlim_HM.start+0.5, latlim_HM.stop-2, 'HM', zorder=10)
     ax.add_patch(
         mpatches.Rectangle(
-            xy = [lonlim_HM[0], latlim_HM[0]], 
-            width = lonlim_HM[1]-lonlim_HM[0], height = latlim_HM[1]-latlim_HM[0],
+            xy = [lonlim_HM.start, latlim_HM.start], 
+            width = lonlim_HM.stop-lonlim_HM.start, height = latlim_HM.stop-latlim_HM.start,
             transform = ccrs.PlateCarree(), fill = False, zorder=10
         )
     )
 
     # TP
-    ax.text(lonlim_TP[0]+0.5, latlim_TP[1]-2, 'TP', zorder=10)
+    ax.text(lonlim_TP.start+0.5, latlim_TP.stop-2, 'TP', zorder=10)
     ax.add_patch(
         mpatches.Rectangle(
-            xy = [lonlim_TP[0], latlim_TP[0]], 
-            width = lonlim_TP[1]-lonlim_TP[0], height = latlim_TP[1]-latlim_TP[0],
+            xy = [lonlim_TP.start, latlim_TP.start], 
+            width = lonlim_TP.stop-lonlim_TP.start, height = latlim_TP.stop-latlim_TP.start,
             transform = ccrs.PlateCarree(), fill = False, zorder=10
         )
     )
