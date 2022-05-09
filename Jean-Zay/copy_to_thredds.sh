02820fa53a5a1a450a3b4c6dcba3bc51dd397d2d
Mickaël <doremiska@gmail.com>
2022-05-09 18:04:22 +0200

    Start preprocess snowCCI

--- Jean-Zay/copy_to_thredds.sh
+++ Jean-Zay/copy_to_thredds.sh
@@ -1,1 +1,15 @@
-aze
+#!/bin/bash
+
+# read -p "Enter path to experiment (IGCM_OUT/...): " path_to_exp
+# read -p "Enter experiment name: " exp
+# read -p "Enter period: " period
+
+path_to_exp="IGCM_OUT/LMDZOR/DEVT/amip"
+exp="LMDZORnudge-STD-NY07"
+period="20040101_20131231"
+
+path=$STORE"/"$path_to_exp"/"$exp
+
+echo $path
+
+mfthredds $path_to_exp"/"exp"/ATM/Analyse/TS_MO" $path"/ATM/Analyse/TS_MO/"$exp"_"$period"_1M_t2m.nc"
